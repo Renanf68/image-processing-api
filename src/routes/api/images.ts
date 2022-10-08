74f8c05ed8d497f110b611b9af94b3e0a9375c95
Renan Costa <renancostam@gmail.com>
2022-10-08 17:37:21 -0300

    Reorganizing functions

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,7 +1,5 @@
-import path from 'path';
 import express from 'express';
-import sharp from 'sharp';
-import { fileExists } from '../../utils';
+import { getProcessedImage } from '../../utils';
 
 // send image thumb if exists OK
 // or create new file, with w and h especified in the query (200x200) OK
@@ -11,41 +9,48 @@ import { fileExists } from '../../utils';
 
 const images = express.Router();
 
+type Query = {
+  [key: string]: string;
+};
+
 images.get('/', async (req, res) => {
   // query
-  const { filename, width, height } = req.query;
-  const widthInt = typeof width === 'string' ? parseInt(width) : 200;
-  const heightInt = typeof height === 'string' ? parseInt(height) : 200;
-  // helpers
-  const inputPath = path.join(
-    __dirname,
-    '../../assets/full',
-    `${filename}.jpg`
-  );
-  const outputFilePath = path.join(
-    __dirname,
-    '../../assets/thumb',
-    `${filename}_thumb.jpg`
-  );
-  // cached
-  try {
-    const isCached = await fileExists(outputFilePath);
-    console.log('isCached: ', isCached);
-    if (isCached) {
-      res.sendFile(outputFilePath);
-      return;
-    }
-    // resizing
-    sharp(inputPath)
-      .resize(widthInt, heightInt)
-      .toFile(outputFilePath, (err) => {
-        if (err) console.log(err);
-        // response
-        res.sendFile(outputFilePath);
-      });
-  } catch (error) {
-    console.log(error);
-  }
+  const { filename, width, height } = req.query as Query;
+  const processedImage = await getProcessedImage(filename, width, height);
+  if (processedImage) res.sendFile(processedImage);
+  else res.send('Erro while processing image');
+  // const widthInt = typeof width === 'string' ? parseInt(width) : 200;
+  // const heightInt = typeof height === 'string' ? parseInt(height) : 200;
+  // // helpers
+  // const inputPath = path.join(
+  //   __dirname,
+  //   '../../assets/full',
+  //   `${filename}.jpg`
+  // );
+  // const outputFilePath = path.join(
+  //   __dirname,
+  //   '../../assets/thumb',
+  //   `${filename}_thumb.jpg`
+  // );
+  // // cached
+  // try {
+  //   const isCached = await fileExists(outputFilePath);
+  //   console.log('isCached: ', isCached);
+  //   if (isCached) {
+  //     res.sendFile(outputFilePath);
+  //     return;
+  //   }
+  //   // resizing
+  //   sharp(inputPath)
+  //     .resize(widthInt, heightInt)
+  //     .toFile(outputFilePath, (err) => {
+  //       if (err) console.log(err);
+  //       // response
+  //       res.sendFile(outputFilePath);
+  //     });
+  // } catch (error) {
+  //   console.log(error);
+  // }
 });
 
 export default images;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,57 @@
+import path from 'path';
 import { promises as fsPromises } from 'fs';
+import sharp from 'sharp';
 
-export const fileExists = async (path: string) => {
+export const fileExists = async (path: string): Promise<boolean> => {
   try {
     await fsPromises.readFile(path);
     return true;
   } catch (error) {
     return false;
+  }
+};
+
+export const resizeImage = async (
+  inputPath: string,
+  width: number,
+  height: number,
+  outputPath: string
+): Promise<sharp.OutputInfo | null> => {
+  try {
+    const process = sharp(inputPath).resize(width, height);
+    return process.toFile(outputPath);
+  } catch (error) {
+    return null;
+  }
+};
+
+export const getProcessedImage = async (
+  filename: string,
+  width: string,
+  height: string
+): Promise<string | null> => {
+  // dimensions
+  const widthInt = width ? parseInt(width) : 200;
+  const heightInt = height ? parseInt(height) : 200;
+  // helpers
+  const inputPath = path.join(__dirname, '../assets/full', `${filename}.jpg`);
+  const outputFilePath = path.join(
+    __dirname,
+    '../assets/thumb',
+    `${filename}_thumb.jpg`
+  );
+  // cached
+  try {
+    const isCached = await fileExists(outputFilePath);
+    console.log('isCached: ', isCached);
+    if (isCached) {
+      return outputFilePath;
+    }
+    // resizing
+    await resizeImage(inputPath, widthInt, heightInt, outputFilePath);
+    return outputFilePath;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 };
