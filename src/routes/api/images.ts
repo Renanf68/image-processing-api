fc20199722e8e52c8c0578ee4d83c90878e1c84a
Renan Costa <renancostam@gmail.com>
2022-10-11 16:48:00 -0300

    Add express types to endpoints requests and responses

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { ImageQuery } from '../../types';
 import { getProcessedImage } from '../../utils/images';
 import logger from '../../utils/logger';
 
 const images = express.Router();
 
-images.get('/', logger, async (req, res) => {
+images.get('/', logger, async (req: Request, res: Response) => {
   const { filename, width, height } = req.query as ImageQuery;
   const processedImage = await getProcessedImage(filename, width, height);
   if (processedImage) res.sendFile(processedImage);
